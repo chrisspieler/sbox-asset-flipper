4298240cb714ed7d54905d0d6b933b352252bbc2
ducc <spielerc@gmail.com>
2023-12-20 23:20:26 -0700

    Disable the buy tool while the model browser is open

--- code/MenuController.cs
+++ code/MenuController.cs
@@ -3,11 +3,14 @@
 public sealed class MenuController : Component
 {
 	[Property] public ModelBrowser ModelMenu { get; set; }
+	[Property] public BuyTool BuyTool { get; set; }
+
 	protected override void OnUpdate()
 	{
 		if ( Input.Pressed( "score" ) )
 		{
 			ModelMenu.Enabled = !ModelMenu.Enabled;
+			BuyTool.Enabled = !ModelMenu.Enabled;
 		}
 	}
 }
