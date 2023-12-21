588140279addfc4b5a90554102ad9a337f3f3c1c
ducc <spielerc@gmail.com>
2023-12-21 16:44:49 -0700

    Add SystemMenuPanel

--- code/MenuController.cs
+++ code/MenuController.cs
@@ -1,18 +1,25 @@
-﻿using System;
-using Sandbox;
+﻿using Sandbox;
 
 public sealed class MenuController : Component
 {
 	[Property] public ModelBrowser ModelMenu { get; set; }
+	[Property] public SystemMenuPanel SystemMenu { get; set; }
 	[Property] public BuyTool BuyTool { get; set; }
+	[Property] public bool IsMenuOpen => ModelMenu.Enabled || SystemMenu.Enabled;
 
 	protected override void OnUpdate()
 	{
 		if ( Input.Pressed( "score" ) )
 		{
 			ModelMenu.Enabled = !ModelMenu.Enabled;
-			BuyTool.Enabled = !ModelMenu.Enabled;
 		}
+		if ( Input.EscapePressed )
+		{
+			SystemMenu.Enabled = !SystemMenu.Enabled;
+		}
+		// TODO: Figure out how to make this work with multiple tools.
+		BuyTool.Enabled = !IsMenuOpen;
+
 		if ( Input.Pressed( "menu" ) )
 		{
 			if ( Input.Down( "run" ) )
